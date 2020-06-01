0a38b1e969fc0be73f0828bb4355deb1a3a5f956
Angad Singh <mail@angad.dev>
2020-06-02 00:16:21 +0530

    Fetch allowed emails from sheet

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,43 +2,61 @@ const passport = require("passport");
 const bcrypt = require("bcrypt");
 const { Strategy: LocalStrategy } = require("passport-local");
 const { PrismaClient } = require("@prisma/client");
-// Replace with spreadsheet
-const emails = require("../emails.json");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const creds = require("../client_secret.json");
 
 const client = new PrismaClient();
+
+let doc;
 
 passport.use(
   new LocalStrategy(
     { usernameField: "email" },
     async (email, password, done) => {
-      const user = await client.user.findOne({
-        where: { email },
-      });
-
-      if (user) {
-        if (await bcrypt.compare(password, user.password)) {
-          return done(null, user);
-        }
-
-        return done("Incorrect password");
-      } else {
-        if (emails.indexOf(email) === -1) {
-          return done("This email is not associated to a build participant");
-        }
-
-        const user_ = await client.user.create({
-          data: { email, password: await bcrypt.hash(password, 14) },
+      try {
+        const user = await client.user.findOne({
+          where: { email },
         });
 
-        // Add donr badge
-        await client.badge.create({
-          data: {
-            User: { connect: { id: user_.id } },
-            img: "https://i.imgur.com/8DmfRD8.png",
-          },
-        });
+        if (user) {
+          if (await bcrypt.compare(password, user.password)) {
+            return done(null, user);
+          }
 
-        return done(null, user_);
+          return done("Incorrect password");
+        } else {
+          if (!doc) {
+            doc = new GoogleSpreadsheet(
+              "1afUKSPP8DHlY8k5viFhlmJjQ4DcQbHvGMmmvUS5UtDw"
+            );
+            await doc.useServiceAccountAuth(creds);
+            await doc.getInfo();
+          }
+
+          const emails = [...(await doc.sheetsByIndex[0].getRows())].map((r) =>
+            r._rawData[0].trim()
+          );
+
+          if (emails.indexOf(email) === -1) {
+            return done("This email is not associated to a build participant");
+          }
+
+          const user_ = await client.user.create({
+            data: { email, password: await bcrypt.hash(password, 14) },
+          });
+
+          // Add donr badge
+          await client.badge.create({
+            data: {
+              User: { connect: { id: user_.id } },
+              img: "https://i.imgur.com/8DmfRD8.png",
+            },
+          });
+
+          return done(null, user_);
+        }
+      } catch (e) {
+        done(e);
       }
     }
   )
