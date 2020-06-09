90c9482a70bb343f64de676c265b282216fb902e
Angad Singh <mail@angad.dev>
2020-06-09 13:36:53 +0530

    Update week 2 timetable

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
 # TheGirlCode #Build2.0 Platform
-
-## User object
-
-```json
-{
-  "name": "",
-  "points": 0,
-  "badges": []
-}
-```
-
-## TODO
-
-- [ ] Deploy to netlify
--- frontend/src/components/TimeTable/two.js
+++ frontend/src/components/TimeTable/two.js
@@ -7,14 +7,14 @@ import { GridT, GridContainer, THV, T, Lesson } from "./helpers";
 const Grid = styled(GridT)`
   grid-template-areas:
     "THV TH1 TH2 TH3 TH4 TH5 TH6 TH7"
-    "TV1 . L2 . L5 L7 . ."
-    "TV2 . L2 . L5 L7 . ."
+    "TV1 . L2 . L6 . . ."
+    "TV2 . L2 . L6 . . ."
     "TV3 . . . . . . ."
-    "TV4 . . . . . . ."
-    "TV5 . . . . . . ."
-    "TV6 L1 . L4 L6 . . ."
-    "TV7 L1 L3 L4 L6 L8 . ."
-    "TV8 . L3 . . L8 . .";
+    "TV4 . L3 . . . . ."
+    "TV5 . L3 . . . . ."
+    "TV6 L1 . L5 L7 . . ."
+    "TV7 L1 L4 L5 L7 . . ."
+    "TV8 . L4 . . . . .";
 `;
 
 export default () => {
@@ -44,34 +44,27 @@ export default () => {
           <b>Review Session</b>
         </Lesson>
         <Lesson area="L2">
-          <b>Lesson 4</b>
-          (English + Hindi)
+          <b>Python Lesson 4</b>
+          (English)
         </Lesson>
         <Lesson area="L3">
           <div>
-            <b>Lesson 4</b>
+            <b>Python Lesson 4</b>
             <br />
-            (English)
+            (Hindi + English)
           </div>
         </Lesson>
         <Lesson area="L4">
-          <b>Review Session</b>
+          <b>Python Lesson 4</b> (English)
         </Lesson>
         <Lesson area="L5">
-          <div>
-            <b>Lesson 2</b> (English): Conditional statements, Lists
-          </div>
+          <b>Review Session</b>
         </Lesson>
         <Lesson area="L6">
           <b>Review Session</b>
         </Lesson>
         <Lesson area="L7">
-          <div>
-            <b>Lesson 3</b> (English): Loops, Built in functions
-          </div>
-        </Lesson>
-        <Lesson area="L8">
-          <b>Introduction</b>
+          <b>Review Session</b>
         </Lesson>
       </Grid>
     </GridContainer>
